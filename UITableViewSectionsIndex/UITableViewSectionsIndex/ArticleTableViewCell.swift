e556f8c26c96c4a96ffaa3ff2ecaf23fe2d167b0
Kirill <kvga923@gmail.com>
2018-02-19 02:13:09 -0500

    UITableView Sections & Index added

---
+++ UITableViewSectionsIndex/UITableViewSectionsIndex/ArticleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewCell.swift
+//  UITableViewSectionsIndex
+//
+//  Created by Kirill G on 2/19/18.
+//  Copyright © 2018 Kirill. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
